97caa20fc0b81a7dfe9ae31ce5ba493dfee58fd3
Garrett Walter <me@garrettwalter.dev>
2021-01-25 19:00:36 -0800

    Start regex-y stuff

--- toc.py
+++ toc.py
@@ -1,23 +1,35 @@
 
+import re
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import Iterable, List, Tuple, Union
+
 import pdfplumber
 
+
 PDF_PATH = 'cs-455_computer-networking.pdf'
+PATTERN = "^(\\d(?:\\.\\d)+)\\s+(.*)\\s+(\\d+)$"
+REGEX = re.compile(PATTERN)
 TOC_START = 18
 TOC_END = 24
 
 
-def extract_lines(page_text: str) -> List[str]:
+TocEntry = Tuple[str, str, int]
+
+
+def extract_lines(page_text: str) -> List[TocEntry]:
     for line in page_text.strip().split('\n'):
-        print(f'"{line}"')
-    return ['a']
+        match = REGEX.match(line)
+        if match is not None:
+            groups = match.groups()
+            print(groups)
+        # print(match)
+        # print(f'"{line}"')
 
 
-def extract_toc(pdf_path, toc_range):
+def extract_toc(pdf_path, toc_pages: Iterable[int]):
     toc_lines = []
     with pdfplumber.open(pdf_path) as pdf:
-        for page_num in range(toc_range):
+        for page_num in toc_pages:
             page = pdf.pages[page_num]
             text = page.extract_text()
             toc_lines.extend(extract_lines(text))
@@ -36,10 +48,10 @@ def extract_toc(pdf_path, toc_range):
     return toc_lines
 
 
-def create_toc_txt(pdf_path: Union[str, Path], toc_range: Tuple[int, int]):
-    toc = extract_toc(pdf_path, toc_range)
+def create_toc_txt(pdf_path: Union[str, Path], toc_pages: Iterable[int]):
+    toc = extract_toc(pdf_path, toc_pages)
     print(toc)
 
 
 if __name__ == '__main__':
-    create_toc_txt(PDF_PATH, (TOC_START, TOC_END + 1))
+    create_toc_txt(PDF_PATH, range(TOC_START, TOC_END + 1))
