68182b0c6ce4699657a1e25e1c269aad375fd2ff
Garrett Walter <me@garrettwalter.dev>
2021-01-25 18:41:11 -0800

    Modularize and flesh out printing toc pages

--- toc.py
+++ toc.py
@@ -1,12 +1,45 @@
 
+from pathlib import Path
+from typing import List, Tuple, Union
 import pdfplumber
 
-pdf_path = 'cs-455_computer-networking.pdf'
-toc_start = 18
-toc_end = 24
+PDF_PATH = 'cs-455_computer-networking.pdf'
+TOC_START = 18
+TOC_END = 24
 
-with pdfplumber.open(pdf_path) as pdf:
-    first_page = pdf.pages[toc_start]
-    # print(first_page.chars[0])
-    first_text = first_page.extract_text()
-    print(first_text)
+
+def extract_lines(page_text: str) -> List[str]:
+    for line in page_text.strip().split('\n'):
+        print(f'"{line}"')
+    return ['a']
+
+
+def extract_toc(pdf_path, toc_range):
+    toc_lines = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for page_num in range(toc_range):
+            page = pdf.pages[page_num]
+            text = page.extract_text()
+            toc_lines.extend(extract_lines(text))
+
+        # page_num = pdf.pages[toc_start]
+        # print(first_page.chars[0])
+
+        # toc_text = page_num.extract_text()
+        # print(toc_text)
+        # for line in toc_text.strip().split('\n'):
+        #     print(f'"{line}"')
+
+        # toc_table = toc_page.extract_tables()
+        # print(toc_table)
+
+    return toc_lines
+
+
+def create_toc_txt(pdf_path: Union[str, Path], toc_range: Tuple[int, int]):
+    toc = extract_toc(pdf_path, toc_range)
+    print(toc)
+
+
+if __name__ == '__main__':
+    create_toc_txt(PDF_PATH, (TOC_START, TOC_END + 1))
